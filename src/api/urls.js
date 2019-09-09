aa8cdd3f1d788f6a882a7f08260ad3dff03dc6b0
Roy Hanley <royhanley8@gmail.com>
2019-09-09 20:17:27 +0100

    ipapi.co integration

--- src/api/urls.js
+++ src/api/urls.js
@@ -1,8 +1,11 @@
 // TODO move to settings file
 const baseURL = "http://localhost:8000";
 
+// backend API
 export const genresURL = `${baseURL}/api/genres/`;
 export const tokenLoginURL = `${baseURL}/auth/token/login/`;
 export const userDetailsURL = `${baseURL}/auth/users/me/`;
 export const userURL = `${baseURL}/auth/users/`;
 
+// ipapi. If we ever go over 1000 calls a day see https://ipapi.co/#pricing
+export const ipapiURL = 'https://ipapi.co/json/';
--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -1,12 +1,14 @@
 import { combineReducers } from 'redux'
 
 import genresReducer from './genres';
+import ipapiReducer from './ipapi';
 import registerReducer from './register';
 import tokenReducer from './token'
 import userReducer from './user';
 
 export default combineReducers({
   genres: genresReducer,
+  ipapi: ipapiReducer,
   register: registerReducer,
   token: tokenReducer,
   user: userReducer,
