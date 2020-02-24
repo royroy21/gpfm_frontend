c227bdd84db1d222a5c33d61a324ef01d1c21a91
roy <royhanley8@gmail.com>
2020-02-24 18:37:04 +0000

    Added gigsURL and locationsURL to urls.js

--- src/api/urls.js
+++ src/api/urls.js
@@ -3,6 +3,8 @@ const baseURL = "http://localhost:8000";
 
 // backend API
 export const genresURL = `${baseURL}/api/genres/`;
+export const gigsURL = `${baseURL}/api/gigs/`;
+export const locationsURL = `${baseURL}/api/locations/`;
 export const tokenLoginURL = `${baseURL}/auth/token/login/`;
 export const userDetailsURL = `${baseURL}/auth/users/me/`;
 export const userURL = `${baseURL}/auth/users/`;
