ef904bc9730e1562a83e3659c8388da74a7aea1c
roy <royhanley8@gmail.com>
2020-02-25 16:01:12 +0000

    Added SingleImage component

---
+++ src/components/SingleImage/Image.js
@@ -0,0 +1,47 @@
+import React from "react";
+import { withStyles } from "@material-ui/core";
+import PropTypes from "prop-types";
+
+const styles = theme => ({
+  large: {
+    margin: "10px auto 5px auto",
+    borderRadius: "25px",
+    width: "100%",
+  },
+  small: {
+    margin: "10px",
+    borderRadius: "10px",
+    width: "50%",
+  },
+});
+
+class Image extends React.Component {
+
+  render() {
+    const { classes, src } = this.props;
+    if (!src){
+      return null
+    }
+    return (
+      <img
+        alt="??"
+        src={src}
+        className={this.props.size  !== "small" ? classes.large : classes.small}
+      />
+    )
+  }
+}
+
+export default withStyles(styles)(Image);
+
+Image.propTypes = {
+  // forceBlank: PropTypes.bool.isRequired,
+  size: PropTypes.string.isRequired,
+  src: PropTypes.string,
+};
+
+Image.defaultProps = {
+  forceBlank: false,
+  size: "small",
+  src: null,
+};
