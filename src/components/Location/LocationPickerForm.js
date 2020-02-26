84998f59a043bf1910457bc8ee45bcbe9b56eff5
roy <royhanley8@gmail.com>
2020-02-26 18:17:35 +0000

    Add full functionality to LocationPicker

--- src/components/Location/LocationPickerForm.js
+++ src/components/Location/LocationPickerForm.js
@@ -1,18 +1,25 @@
-import React, {Fragment} from "react";
+import React, {createRef} from "react";
 import TextField from "@material-ui/core/TextField";
 import {withStyles} from "@material-ui/core";
 import ForwardGeocodingWrapper from "./wrapper";
 import PropTypes from "prop-types";
 import Form from "../Form";
-import List from "@material-ui/core/List";
-import ListItem from "@material-ui/core/ListItem";
-import ListItemText from "@material-ui/core/ListItemText";
 import Paper from "@material-ui/core/Paper";
+import Downshift from "downshift";
+import MenuItem from "@material-ui/core/MenuItem";
+
+// TODO -
+// add button for start get location logic
+// add indication to user if location is selected or not
+// add country dropdown
+// (nice to have) add autocomplete from locations api
 
 const styles = theme => ({
   container: {
-    display: "flex",
-    margin: `${theme.spacing(3)}px 0 0 0`,
+    marginTop: theme.spacing(2),
+    marginBottom: theme.spacing(2),
+    flexGrow: 1,
+    position: 'relative',
   },
   searchField: {
     width: "100%",
@@ -33,9 +40,21 @@ const styles = theme => ({
   noLocationsFoundError: {
     color: theme.palette.secondary.main,
   },
+  paper: {
+    maxHeight: '225px',
+    overflow: 'scroll',
+    position: 'absolute',
+    zIndex: 2,
+    marginTop: theme.spacing(1),
+    left: 0,
+    right: 0,
+    overflowX: 'hidden',
+  },
 });
 
 class LocationPickerForm extends Form {
+
+  optionsPaperRef = createRef();
 
   DEFAULT_LABEL = "Gig Location";
 
@@ -47,6 +66,7 @@ class LocationPickerForm extends Form {
       country: "gb",
     },
     selectedLocation: null,
+    menuIsOpen: false,
   };
 
   componentDidMount() {
@@ -54,19 +74,39 @@ class LocationPickerForm extends Form {
     if (location) {
       this.setLocation([location]);
     }
+    document.addEventListener("keydown", this.escFunction, false);
+  }
+
+  componentWillUnmount(){
+    document.removeEventListener("keydown", this.escFunction, false);
   }
 
   componentDidUpdate(prevProps, prevState, snapshot) {
+    const { objects: locations } = this.props.store.forwardGeocoding;
+    if (
+      !this.state.menuIsOpen
+      && !this.state.formData.q
+      && locations && locations.length > 1
+    ) {
+      this.setState({menuIsOpen: true})
+    }
     const { object: location } = this.props.store.location;
     if (location) {
       this.props.updateLocationField(location.id);
+      if (this.state.menuIsOpen) {
+        this.setState({menuIsOpen: false});
+        this.props.actions.clearForwardGeocoding()
+      }
     }
-
     const { objects } = this.props.store.forwardGeocoding;
     if (prevProps.store.forwardGeocoding.loading && objects && objects.length) {
       this.setLocation(objects);
     }
-    if (prevProps.store.forwardGeocoding.loading && (!objects || !objects.length) && prevState.selectedLocation !== null) {
+    if (
+      prevProps.store.forwardGeocoding.loading
+      && (!objects || !objects.length)
+      && prevState.selectedLocation !== null
+    ) {
       this.setState({selectedLocation: null});
       this.props.updateLocationField(null);
       this.props.actions.clearLocation();
@@ -89,6 +129,9 @@ class LocationPickerForm extends Form {
 
   setLocation = (locations) => {
     const location = locations[0];
+    if (!location){
+      return null;
+    }
     if (this.state.formData.q === location.name) {
       return null;
     }
@@ -113,69 +156,100 @@ class LocationPickerForm extends Form {
     }
   };
 
-  getMultipleLocationsList() {
-    if (this.state.selectedLocation) {
-      return null
+  renderOptions(optionsProps) {
+    const {
+      option,
+      index,
+      itemProps,
+      highlightedIndex,
+      selectedItem,
+    } = optionsProps;
+    const isHighlighted = highlightedIndex === index;
+    if (isHighlighted) {
+      this.scrollToHighlightedOption(option.name)
     }
-    const { classes } = this.props;
-    const { objects } = this.props.store.forwardGeocoding;
-    if (objects && objects.length > 1) {
-      return (
-        <div className={classes.multipleLocationsContainer}>
-          <Paper className={classes.multipleLocationsPaper}>
-            <List
-              component={"nav"}
-              className={classes.multipleLocationsList}
-            >
-              {objects.map(location => (
-                <ListItem
-                  button
-                  key={location.name}
-                  onClick={() => this.setLocation([location])}
-                >
-                  <ListItemText
-                    secondary={location.name}
-                  />
-                </ListItem>
-              ))}
-            </List>
-          </Paper>
-        </div>
-      )
-    }
+    const isSelected = selectedItem ? selectedItem.name === option.name : false;
+    return (
+      <MenuItem
+        {...itemProps}
+        key={option.name}
+        selected={isHighlighted}
+        component={"div"}
+        style={{
+          fontWeight: isSelected ? 500 : 400,
+        }}
+      >
+        {option.name}
+      </MenuItem>
+    );
   }
 
-  showNoLocationsFoundError() {
-    const { objects } = this.props.store.forwardGeocoding;
-    const { classes } = this.props;
-    if (!objects || !objects.length) {
-      return (
-        <p className={classes.noLocationsFoundError}>
-          {"No Locations Found"}
-        </p>
-      )
+  scrollToHighlightedOption(optionName) {
+    const { objects: locations } = this.props.store.forwardGeocoding;
+    const y = locations.map(location => location.name).indexOf(optionName) * 48;
+    this.optionsPaperRef.current.scrollTo(0, y);
+  };
+
+  onOuterClick = () => {
+    this.props.actions.clearForwardGeocoding();
+    this.setState({menuIsOpen: false});
+  };
+
+  escFunction = (event) => {
+    if(event.keyCode === 27) {
+      this.onOuterClick();
     }
   };
 
   getFields() {
     const { classes } = this.props;
+    const { objects: locations } = this.props.store.forwardGeocoding;
     return (
-      <Fragment>
-        <div className={classes.container}>
-          <TextField
-            className={classes.searchField}
-            id={"get-location"}
-            label={this.DEFAULT_LABEL}
-            variant={"outlined"}
-            onChange={this.handleChange}
-            value={this.state.formData.q}
-            // TODO - implement this?
-            onKeyPress={this.onInputFieldKeyPress}
-          />
-        </div>
-        {this.getMultipleLocationsList()}
-        {this.showNoLocationsFoundError()}
-      </Fragment>
+      <Downshift
+        onChange={location => this.setLocation([location])}
+        itemToString={item => (item ? item.name : '')}
+        isOpen={this.state.menuIsOpen}
+        onOuterClick={this.onOuterClick}
+      >
+        {({
+          getInputProps,
+          getItemProps,
+          isOpen,
+          highlightedIndex,
+          selectedItem,
+        }) => (
+          <div className={classes.container}>
+            <TextField
+              {...getInputProps()}
+              className={classes.searchField}
+              id={"get-location"}
+              label={this.DEFAULT_LABEL}
+              variant={"outlined"}
+              onChange={this.handleChange}
+              value={this.state.formData.q}
+            />
+            {isOpen ? (
+              <Paper
+                className={classes.paper}
+                ref={this.optionsPaperRef}
+                square
+              >
+                {(locations || []).map((option, index) =>
+                  this.renderOptions({
+                    option,
+                    index,
+                    itemProps: getItemProps({
+                      item: option,
+                      id: `downshift-multiple-item-${option.name}`,
+                    }),
+                    highlightedIndex,
+                    selectedItem,
+                  }),
+                )}
+              </Paper>): null}
+          </div>
+        )}
+      </Downshift>
     )
   }
 }
